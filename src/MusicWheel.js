6969977adaeac08fea1dc3bde3e753bf5f8ec4d6
brajeshgarg468 <gargbrajesh468@gmail.com>
2023-03-11 17:34:05 +0530

    video player edit

--- src/MusicWheel.js
+++ src/MusicWheel.js
@@ -104,7 +104,7 @@ function MusicWheel(props) {
   // const [imageTypeActive, setImageTypeActive] = useState(1);
   const [imageTypeActive, setImageTypeActive] = useState(false);
   const [durationDataIndex, setDurationDataIndex] = useState(0);
-  const [durationValue, setDurationValue] = useState('');
+  const [durationValue, setDurationValue] = useState("");
   const [disable, setDisable] = useState(false);
   const [freeActive, setFreeActive] = useState(false);
   const [disableIntensity, setDisableIntensity] = useState(false);
@@ -118,121 +118,452 @@ function MusicWheel(props) {
   const packageData = ["", "F", "P", "P", "F"];
   const packageName = ["", "Mix", "Premium", "Original"];
   const imageTypeData = ["", "Keys", "Letter", "Staff"];
-  const durationData = {minute1:[104,
-  105,
-  106,
-  107,
-  108,
-  109,
-  120,
-  121,
-  125,
-  126,
-  127,
-  128,
-  129,
-  135,
-  139,
-  141,
-  145,
-  147,
-  150,
-  151,
-  155,
-  156,
-  160,
-  167,
-  169,
-  170,
-  173,
-  177,
-  178,
-  178,
-  178,
-  178,179,],
- 
-minute2:[ 181,
-  182,
-  183,
-  184,
-  178,
-  184,
-  185,
-  191,
-  192,
-  193,
-  194,
-  195,
-  196,
-  198,
-  199,
-  200,
-  202,
-  203,
-  204,
-  206,
-  207,
-  208,
-  209,
-  210,
-  211,
-  212,
-  213,
-  213,
-  214,
-  215,
-  216,
-  217,
-  218,
-  220,
-  222,
-  223,
-  225,
-  226,
-  231,
-  233,
-  234,
-  235,
-  236,
-  237,
-  238,
-  240,],  
-  minute3:[
-  241,
-    242,
-    243,
-    244,
-    245,
-    246,
-    247,
-    248,
-    249,
-    250,
-    251,
-    252,
-    253,
-    254,
-    256,
-    259,
-    261,
-    263,
-    265,
-    278,
-    280,
-    285,
-    288,
-    300,
-    301,
-    320,
-    333,
-    340,
-    350,
-    356,
-    360,
-    402,
-    437,
-    533
-  ],
+  const durationData = {
+    minute1: [
+      0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,
+          18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34,
+          35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51,
+          52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68,
+          69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85,
+          86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102,
+          103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116,
+          117, 118, 119, 120,
+    ],
+
+    minute2: [
+      121,
+      122,
+      123,
+      124,
+      125,
+      126,
+      127,
+      128,
+      129,
+      130,
+      131,
+      132,
+      133,
+      134,
+      135,
+      136,
+      137,
+      138,
+      139,
+      140,
+      141,
+      142,
+      143,
+      144,
+      145,
+      146,
+      147,
+      148,
+      149,
+      150,
+      151,
+      152,
+      153,
+      154,
+      155,
+      156,
+      157,
+      158,
+      159,
+      160,
+      161,
+      162,
+      163,
+      164,
+      165,
+      166,
+      167,
+      168,
+      169,
+      170,
+      171,
+      172,
+      173,
+      174,
+      175,
+      176,
+      177,
+      178,
+      179,
+      180,
+      181,
+      182,
+      183,
+      184,
+      185,
+      186,
+      187,
+      188,
+      189,
+      190,
+      191,
+      192,
+      193,
+      194,
+      195,
+      196,
+      197,
+      198,
+      199,
+      200,
+      201,
+      202,
+      203,
+      204,
+      205,
+      206,
+      207,
+      208,
+      209,
+      210,
+      211,
+      212,
+      213,
+      214,
+      215,
+      216,
+      217,
+      218,
+      219,
+      220,
+      221,
+      222,
+      223,
+      224,
+      225,
+      226,
+      227,
+      228,
+      229,
+      230,
+      231,
+      232,
+      233,
+      234,
+      235,
+      236,
+      237,
+      238,
+      239,
+      240,
+    ],
+    minute3: [
+      241,
+      242,
+      243,
+      244,
+      245,
+      246,
+      247,
+      248,
+      249,
+      250,
+      251,
+      252,
+      253,
+      254,
+      255,
+      256,
+      257,
+      258,
+      259,
+      260,
+      261,
+      262,
+      263,
+      264,
+      265,
+      266,
+      267,
+      268,
+      269,
+      270,
+      271,
+      272,
+      273,
+      274,
+      275,
+      276,
+      277,
+      278,
+      279,
+      280,
+      281,
+      282,
+      283,
+      284,
+      285,
+      286,
+      287,
+      288,
+      289,
+      290,
+      291,
+      292,
+      293,
+      294,
+      295,
+      296,
+      297,
+      298,
+      299,
+      300,
+      301,
+      302,
+      303,
+      304,
+      305,
+      306,
+      307,
+      308,
+      309,
+      310,
+      311,
+      312,
+      313,
+      314,
+      315,
+      316,
+      317,
+      318,
+      319,
+      320,
+      321,
+      322,
+      323,
+      324,
+      325,
+      326,
+      327,
+      328,
+      329,
+      330,
+      331,
+      332,
+      333,
+      334,
+      335,
+      336,
+      337,
+      338,
+      339,
+      340,
+      341,
+      342,
+      343,
+      344,
+      345,
+      346,
+      347,
+      348,
+      349,
+      350,
+      351,
+      352,
+      353,
+      354,
+      355,
+      356,
+      357,
+      358,
+      359,
+      360,
+      361,
+      362,
+      363,
+      364,
+      365,
+      366,
+      367,
+      368,
+      369,
+      370,
+      371,
+      372,
+      373,
+      374,
+      375,
+      376,
+      377,
+      378,
+      379,
+      380,
+      381,
+      382,
+      383,
+      384,
+      385,
+      386,
+      387,
+      388,
+      389,
+      390,
+      391,
+      392,
+      393,
+      394,
+      395,
+      396,
+      397,
+      398,
+      399,
+      400,
+      401,
+      402,
+      403,
+      404,
+      405,
+      406,
+      407,
+      408,
+      409,
+      410,
+      411,
+      412,
+      413,
+      414,
+      415,
+      416,
+      417,
+      418,
+      419,
+      420,
+      421,
+      422,
+      423,
+      424,
+      425,
+      426,
+      427,
+      428,
+      429,
+      430,
+      431,
+      432,
+      433,
+      434,
+      435,
+      436,
+      437,
+      438,
+      439,
+      440,
+      441,
+      442,
+      443,
+      444,
+      445,
+      446,
+      447,
+      448,
+      449,
+      450,
+      451,
+      452,
+      453,
+      454,
+      455,
+      456,
+      457,
+      458,
+      459,
+      460,
+      461,
+      462,
+      463,
+      464,
+      465,
+      466,
+      467,
+      468,
+      469,
+      470,
+      471,
+      472,
+      473,
+      474,
+      475,
+      476,
+      477,
+      478,
+      479,
+      480,
+      481,
+      482,
+      483,
+      484,
+      485,
+      486,
+      487,
+      488,
+      489,
+      490,
+      491,
+      492,
+      493,
+      494,
+      495,
+      496,
+      497,
+      498,
+      499,
+      500,
+      501,
+      502,
+      503,
+      504,
+      505,
+      506,
+      507,
+      508,
+      509,
+      510,
+      511,
+      512,
+      513,
+      514,
+      515,
+      516,
+      517,
+      518,
+      519,
+      520,
+      521,
+      522,
+      523,
+      524,
+      525,
+      526,
+      527,
+      528,
+      529,
+      530,
+      531,
+      532,
+      533,
+      534,
+      535,
+      536,
+      537,
+      538,
+      539,
+      540,
+      541,
+      542,
+      543,
+      544,
+      545,
+      546,
+      547,
+      548,
+      549,
+      550,
+    ],
   };
   let nordArray = [];
   const arryOne = ["c3", "c2", "c1"];
@@ -362,10 +693,10 @@ minute2:[ 181,
         setPackageDataIndex(packageDataIndex);
         setFreeActive(true);
         setDisable(true);
-        setImageTypeActive('');
+        setImageTypeActive("");
         setDisableIntensity(true);
         setImageTypeIndex("");
-        setPackageDataIndex('')
+        setPackageDataIndex("");
         setGroups("f1,f2");
         setNord({ c1: [], c2: [], c3: [] });
         setNordData([]);
@@ -384,13 +715,12 @@ minute2:[ 181,
     }
     if (type == "Premium") {
       setSubscriptionActivePlan(3);
-      if(packageDataIndex >2){
+      if (packageDataIndex > 2) {
         setGroups("O1,O2,O3");
-      }
-      else{
+      } else {
         setGroups("G1,G2,G3,G4");
       }
-     
+
       setImageTypeActive(1);
       setImageTypeIndex("Letter");
       // setPackageDataIndex(1);
@@ -399,14 +729,13 @@ minute2:[ 181,
       setFreeActive(false);
       // setImageTypeIndex("Letter,Staff,Keys");
     }
-    
   }
   // function intensitySort(){
   //   setIntensityIndex(1+intensityIndex);
 
   //   if(intensityIndex == '1')
   //   {
-      
+
   //     fetchSongsData()
   //   }
   //   setNord({ c1: [], c2: [], c3: [] });
@@ -449,12 +778,10 @@ minute2:[ 181,
           setImageTypeIndex("Letter");
         }
         if (packageDataIndex == "3") {
-         
           setGroups("G1,G2,G3,G4,o1,o2,o3");
           setSubscriptionActivePlan(3);
           setImageTypeIndex("Letter");
           setImageTypeActive(1);
-          
         }
 
         setNord({ c1: [], c2: [], c3: [] });
@@ -498,26 +825,26 @@ minute2:[ 181,
         // setDurationValue(durationData["minute1"]);
       } else {
         setDurationDataIndex(durationDataIndex + 1);
-       
-        if(durationDataIndex==1)
-        {
-          setDurationValue(durationData["minute1"])
+
+        if (durationDataIndex == '0') {
+          setDurationValue(durationData["minute1"]);
+          console.log(durationValue, "durationValue");
         }
-       
-        if(durationDataIndex==2)
-        {
-          setDurationValue(durationData["minute2"])
+
+        if (durationDataIndex == '1') {
+          setDurationValue(durationData["minute2"]);
+          console.log(durationValue, "durationValue");
         }
-        if(durationDataIndex==3)
-        {
-          setDurationValue(durationData["minute3"])
+        if (durationDataIndex == '2') {
+          setDurationValue(durationData["minute3"]);
+          console.log(durationValue, "durationValue");
         }
-        if(durationDataIndex==4)
-        {
-          setDurationDataIndex(1);
-          setDurationValue('')
+        if (durationDataIndex == '3') {
+          setDurationDataIndex(0);
+          setDurationValue("");
+          console.log(durationValue, "durationValue");
         }
-       
+
         setNord({ c1: [], c2: [], c3: [] });
         setNordData([]);
       }
@@ -910,25 +1237,14 @@ minute2:[ 181,
               }}
             >
               {durationDataIndex == 1 ? (
-                  <Image src={Duration1} alt="..." />
-                ) : durationDataIndex == 2 ?
-                  <Image
-                    src={Duration2}
-                    alt="..."
-                    width="200px"
-                    height="200px"
-                  />
-                 : durationDataIndex == 3 ? (
-                  <Image
-                    src={Duration3}
-                    alt="..."
-                    width="200px"
-                    height="200px"
-                  />
-                ) : (
-                  <Image src={DurationDefault} alt="..." />
-                )
-             }{" "}
+                <Image src={Duration1} alt="..." />
+              ) : durationDataIndex == 2 ? (
+                <Image src={Duration2} alt="..." width="200px" height="200px" />
+              ) : durationDataIndex == 3 ? (
+                <Image src={Duration3} alt="..." width="200px" height="200px" />
+              ) : (
+                <Image src={DurationDefault} alt="..." />
+              )}{" "}
             </button>
           </Grid>
           <Grid item xs={8} md={8} className={classes.wheelContianer}>
@@ -1081,10 +1397,10 @@ minute2:[ 181,
                   : allPlaySongsDuration}
                 {songsData && songsData.length > 0
                   ? songsData.map((val, ind) =>
-                      console.log(
-                        (totalduraion =
-                          parseInt(totalduraion) + parseInt(val["duration"]))
-                      )
+                     
+                        console.log((totalduraion =
+                          parseInt(totalduraion) + parseInt(val["duration"])))
+                      
                     )
                   : ""}
               </p>
@@ -1277,12 +1593,12 @@ minute2:[ 181,
               <th className={classes.thStyle}>Composer</th>
               <th className={classes.thStyle}>Note</th>
               <th className={classes.thStyle}>Tempo</th>
-              <th className={classes.thStyle}>Img</th>
+              <th className={classes.thStyle}>dur..</th>
               <th
                 className={classes.thStyle}
                 style={{ cursor: "pointer" }}
                 onClick={(e) => {
-                  btnHandler(type, e, ind)
+                  btnHandler(type, e, ind);
                   fetchSongsData();
                 }}
               >
@@ -1290,7 +1606,7 @@ minute2:[ 181,
               </th>
             </tr>
             {songsData && songsData.length > 0
-              ? songsData.map((val, ind) =>
+              ? songsData.map((val, ind) => (
                   // val["song_name"].includes("_P.") ? (
                   //   <tr
                   //     key={"songs" + ind}
@@ -1309,21 +1625,21 @@ minute2:[ 181,
                   //     <td>{val["intensity"]}</td>
                   //   </tr>
                   // ) : (
-                    <tr
-                      key={"songs" + ind}
-                      onClick={() => handleClickSong(songsData, ind)}
-                      className={styles.liststyle}
-                    >
-                      <td>{val["song_title"]}</td>
-                      <td>{val["composer"]}</td>
-                      <td>{val["note_or_cord"]}</td>
-                      <td>{val["tempo"]}</td>
-                      <td>{val["image_type"]}</td>
-                      <td>{val["intensity"]}</td>
-                    </tr>
-                  )
-                // )
-              : "No Songs Found"}
+                  <tr
+                    key={"songs" + ind}
+                    onClick={() => handleClickSong(songsData, ind)}
+                    className={styles.liststyle}
+                  >
+                    <td>{val["song_title"]}</td>
+                    <td>{val["composer"]}</td>
+                    <td>{val["note_or_cord"]}</td>
+                    <td>{val["tempo"]}</td>
+                    <td>{val["duration"]}</td>
+                    <td>{val["intensity"]}</td>
+                  </tr>
+                ))
+              : // )
+                "No Songs Found"}
           </table>
 
           {/* {songsData && songsData.length > 0 ? (
--- src/VideoPlayer.js
+++ src/VideoPlayer.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { setCookie } from "cookies-next";
 import { Grid } from "@mui/material";
+import Image from "next/image";
 import { makeStyles } from "@material-ui/core";
 import ReactPlayer from "react-player";
+import Guitar from "../public/assets/images/guitar.jpg";
 import dynamic from "next/dynamic";
 // import 'shaka-player/dist/controls.css';
 // const ShakaPlayer = dynamic(() => import('shaka-player-react'), { ssr: false });
@@ -20,6 +22,7 @@ const useStyles = makeStyles({
     padding: "20px",
     height: "90vh",
     marginTop: "8px",
+    position:'relative',
     "@media (min-width: 1280px) and (max-width:1480px)": { height: "130vh" },
     "@media (min-width: 1481px) and (max-width:1920px)": { height: "110vh" },
     "@media (min-width: 770px) and (max-width:1024px)": {
@@ -36,6 +39,18 @@ const useStyles = makeStyles({
     "@media (max-width:700px)": {
       margin: "10px 15px 15px 28px !important",
       height: "25vh",
+    },
+  },
+  playerDisplay:{
+    objectFit: "cover",
+    backgroundSize: "cover",
+    marginTop: "-400px",
+    // "@media (min-width: 1280px) and (max-width:1480px)": {  marginTop: "-300px", },
+    "@media (min-width: 1280px) and (max-width:1680px)": {
+      marginTop: "-180px",
+    },
+    "@media (min-width: 720px) and (max-width:1024px)": {
+      marginTop: "-400px",
     },
   },
 });
@@ -59,21 +74,23 @@ function VideoPlayer(prop) {
   const classes = useStyles();
   return (
     <div>
-      <Grid container spacing={2}>
-        <Grid item xs={12} md={12} className={classes.videoCard}>
+      <Grid container spacing={2} style={{display:'block',position:'relative'}}>
+        <Grid item xs={12} md={12} className={classes.videoCard} >
           {prop.musicData ? (
             <ReactPlayer
               controls
               url={music}
-              width="100%"
-              height="100%"
+              width="100vw"
+              height="80vh"
+             className={classes.playerDisplay}
               playing={true}
               fluid={true}
               onEnded={playNextSong}
-              style={{ marginTop: "-50px" }}
+              // style={{ marginTop: "-50px" }}
               config={{ file: { attributes: { controlsList: 'nodownload' } } }}
              
             />
+
           ) : (
             <ReactPlayer
               controls
@@ -84,7 +101,9 @@ function VideoPlayer(prop) {
               loop={true}
             />
           )}
+          {/* <div style={{position:'absolute',bottom:'0',height:'300px',}}><Image src={Guitar} alt="..."/></div> */}
         </Grid>
+        
       </Grid>
     </div>
   );
