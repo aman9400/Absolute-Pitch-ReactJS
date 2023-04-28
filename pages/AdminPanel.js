53a99f01041b1659eaa1b1ce17f44ec56d253be6
brajeshgarg468 <gargbrajesh468@gmail.com>
2023-04-28 12:20:05 +0530

    add modal

--- pages/AdminPanel.js
+++ pages/AdminPanel.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import { Grid, makeStyles, Button, Container } from "@material-ui/core";
 import Image from "next/image";
 import TipsImg from "../public/assets/images/tips.jpg";
@@ -7,6 +7,22 @@ import Gmail from "../public/assets/images/mail.png";
 import Twitter from "../public/assets/images/twitter.png";
 import InstaGram from "../public/assets/images/insta.png";
 import FaceBook from "../public/assets/images/facebook.png";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import UserLog from "./UserLog";
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '45%',
+  transform: 'translate(-50%, -50%)',
+  width: '100%',
+  height:'100%',
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 const useStyles = makeStyles({
   root: {
     position: "relative",
@@ -83,9 +99,21 @@ const useStyles = makeStyles({
     cursor: "pointer",
     borderRadius: "5px",
   },
+  LocationBox: {
+    height: "150px",
+    marginTop: "20px",
+    display: "flex",
+    alignItems: "center",
+    JustifyContent: "center",
+    cursor: "pointer",
+    borderRadius: "5px",
+  },
 });
 const AdminPanel = () => {
   const classes = useStyles();
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   return (
     <Grid container spacing={0} className={classes.containerBox}>
       <Grid
@@ -144,9 +172,48 @@ const AdminPanel = () => {
         sm={12}
         sx={12}
         className={classes.leftSection}
-      ></Grid>
-      <Grid item xs={12} md={3} sm={12} sx={12} className={classes.leftSection}>
-        <Grid container spacing={0} className={classes.containerBox}>
+        style={{ border: "2px solid red" }}
+      >
+        <div
+          className={classes.systemBox}
+          style={{
+            border: "2px solid red",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <h1 className={classes.typo_design}>My Library</h1>
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={style}>
+              <UserLog/>
+            </Box>
+          </Modal>
+          <Button
+          onClick={handleOpen}
+            style={{
+              marginLeft: "32%",
+              background: "#fff",
+              borderRadius: "10px",
+              width: "20%",
+              padding: "10px",
+            }}
+          >
+            Click Here
+          </Button>
+        </div>
+      </Grid>
+      <Grid item xs={12} md={3} sm={12} sx={12}>
+        <Grid
+          container
+          spacing={0}
+          className={classes.SocialLinkBox}
+          style={{ border: "2px solid red" }}
+        >
           <Grid
             item
             xs={12}
@@ -156,7 +223,11 @@ const AdminPanel = () => {
             className={classes.SocialLinkBox}
             style={{ height: "70px", padding: "3px" }}
           >
-            <a href="https://www.instagram.com/" target="new" title="Soundcloud">
+            <a
+              href="https://www.instagram.com/"
+              target="new"
+              title="Soundcloud"
+            >
               <Image
                 src={InstaGram}
                 alt="Picture of the author"
@@ -210,7 +281,11 @@ const AdminPanel = () => {
             className={classes.SocialLinkBox}
             style={{ height: "70px", padding: "3px" }}
           >
-            <a href="https://twitter.com/i/flow/login" target="new" title="Soundcloud">
+            <a
+              href="https://twitter.com/i/flow/login"
+              target="new"
+              title="Soundcloud"
+            >
               <Image
                 src={Twitter}
                 alt="Picture of the author"
@@ -220,6 +295,12 @@ const AdminPanel = () => {
             </a>
           </Grid>
         </Grid>
+        <div
+          className={classes.LocationBox}
+          style={{ border: "2px solid red" }}
+        >
+          hjhjjjjj
+        </div>
       </Grid>
     </Grid>
   );
