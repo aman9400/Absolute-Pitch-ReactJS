2e2a8de923f29dc24a605e4bc449f5367b74bc04
brajeshgarg468 <gargbrajesh468@gmail.com>
2023-06-08 21:15:55 +0530

    add new graph api

--- pages/NewChart.js
+++ pages/NewChart.js
@@ -1,10 +1,51 @@
 import React ,{useState,useEffect}from 'react'
+import Tooltips from '@mui/material/Tooltip';
 import {ResponsiveContainer,LineChart,Line,XAxis,YAxis,Tooltip,CartesianGrid,Legend} from "recharts"
 const NewChart = () => {
+
+  const [nordData, setNordData] = useState([]);
+  useEffect(() => {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+    myHeaders.append("Cookie", "PHPSESSID=6fdb4183dbaa64275807f0849965500a");
+    
+    var urlencoded = new URLSearchParams();
+    urlencoded.append("report-graph", "1");
+    urlencoded.append("user_id", "10");
+    urlencoded.append("start_date", "2023-05-01");
+    urlencoded.append("end_date", "2023-05-30");
+    
+    var requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: urlencoded,
+      redirect: 'follow'
+    };
+    
+    fetch("https://mylatinhome.com/absolute/appdata/webservice.php", requestOptions)
+  .then((response) => response.json())
+  .then((responseJson) => {
+    if (responseJson.success === "0") {
+      alert("report data");
+    } else {
+      setNordData(responseJson.data);
+     
+    }
+  });
+
+  
+
+}, [nordData]);
+let newData = {};
+for(let i=0; i <= nordData.length; i++)
+{
+newData[i] = nordData[i]
+}
+console.log(newData,'new...')
     const data = [
         {
           time: '1',
-          date:'1'
+          date:'2'
         },
         {
           time: '2',
@@ -12,11 +53,11 @@ const NewChart = () => {
         },
         {
           time: '3',
-          date:'2'
+          date:'4'
         },
         {
           time: '4',
-          date:'7'
+          date:'4'
         },
         {
           time: '5',
@@ -70,12 +111,12 @@ const NewChart = () => {
       };
   return (
     <div style={{background:'#fff',borderRadius:'10px'}}>
-      <ResponsiveContainer width='79%' aspect={3}>
-        <LineChart data={data} width={930} height={200} margin={{top:25,right:20,left:20,bottom:20}}>
+      <ResponsiveContainer width='95%' aspect={3}>
+        <LineChart data={nordData} width={930} height={200} margin={{top:25,right:20,left:20,bottom:20}}>
         <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey='time' />
-            <YAxis />
-            <Tooltip style={{width:'100px !important'}}/>
+            <XAxis dataKey='date' />
+            <YAxis dataKey='time'/>
+            <Tooltip dataKey='time'/>
             <Legend />
             {/* <Line dataKey='amt' stroke='red' activeDot={{r:8}} /> */}
             <Line dataKey='date' stroke='green' activeDot={{r:8}} options={options}/>
