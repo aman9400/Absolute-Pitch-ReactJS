dc0f8ff7dbf70b8ff44de3b4a0acede9fad97560
brajeshgarg468 <gargbrajesh468@gmail.com>
2023-06-08 21:37:42 +0530

    new update

--- pages/NewChart.js
+++ pages/NewChart.js
@@ -45,11 +45,11 @@ newData[i] = nordData[i]
 console.log(newData,'new...')
     const data = [
         {
-          time: '1',
+          time: '5',
           date:'2023-05-01'
         },
         {
-          time: '2',
+          time: '7',
           date:'2023-05-02'
         },
         {
@@ -57,11 +57,11 @@ console.log(newData,'new...')
           date:'2023-05-03'
         },
         {
-          time: '4',
+          time: '5',
           date:'2023-05-04'
         },
         {
-          time: '5',
+          time: '8',
           date:'2023-05-05'
         },
         {
@@ -69,58 +69,42 @@ console.log(newData,'new...')
           date:'2023-05-06'
         },
         {
-          time: '7',
+          time: '9',
           date:'2023-05-07'
         },
         {
-          time: '8',
+          time: '4',
           date:'2023-05-08'
         },
         {
-          time: '9',
+          time: '6',
           date:'2023-05-09'
         },
         {
-          time: '10',
+          time: '3',
           date:'2023-05-10'
         },
         {
-          time: '11',
+          time: '9',
           date:'2023-05-11'
         },
         {
-          time: '12',
+          time: '2',
           date:'2023-05-12'
         },
       ];
-      const options = {
-        title: {
-          display: true,
-          text: "Number of downloads of our app"
-        },
-        scales: {
-          yAxes: [
-            {
-              ticks: {
-                min: 0,
-                max: 20,
-                stepSize: 3
-              }
-            }
-          ]
-        }
-      };
+     
   return (
     <div style={{background:'#fff',borderRadius:'10px'}}>
       <ResponsiveContainer width='95%' aspect={3}>
-        <LineChart data={nordData} width={930} height={200} margin={{top:25,right:20,left:20,bottom:20}}>
+        <LineChart data={data} width={930} height={200} margin={{top:25,right:20,left:20,bottom:20}}>
         <CartesianGrid strokeDasharray="3 3" />
             <XAxis dataKey='date' />
             <YAxis dataKey='time'/>
-            <Tooltip dataKey='time'/>
+            <Tooltip/>
             <Legend />
             {/* <Line dataKey='amt' stroke='red' activeDot={{r:8}} /> */}
-            <Line dataKey='date' stroke='green' activeDot={{r:8}} options={options}/>
+            <Line dataKey='time' stroke='green' activeDot={{r:8}} />
             
         </LineChart>
       </ResponsiveContainer>
